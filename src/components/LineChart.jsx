dc31b01dc783e9b65a3fd7114d1c5ea5ad482efb
Shahid Afridi <shafridi9807@gmail.com>
2022-07-29 21:15:36 +0530

    chart fixed

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
+import 'chart.js/auto';
 import { Row, Col, Typography } from 'antd';
 const { Title } = Typography
 const LineChart = ({coinHistory, currentPrice, coinName}) => {
